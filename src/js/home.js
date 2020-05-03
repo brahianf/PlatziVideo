c892b644d905e30c0bebceef75f2707f5770a65a
brahianf <brahianf@unicauca.edu.co>
2020-05-03 01:15:02 -0500

    Metodos ClassList y style para manipular html y css

--- src/js/home.js
+++ src/js/home.js
@@ -65,6 +65,15 @@ fetch('https://randomuser.me/api/')
     const data = await response.json()
     return data
   }
+  const $form = document.getElementById('form')
+  const $home = document.getElementById('home')
+
+  $form.addEventListener('submit', (event) => {
+    // debugger
+    event.preventDefault
+    $home.classList.add('search-active')
+    // Agregar Clases en html
+  })
   
   const actionList= await getData('https://yts.mx/api/v2/list_movies.json?genre=action')
   const dramaList= await getData('https://yts.mx/api/v2/list_movies.json?genre=drama')
@@ -86,8 +95,6 @@ fetch('https://randomuser.me/api/')
   const $dramaContainer = document.querySelector('#drama')
   const $animationContainer = document.getElementById('animation')
   const $featuringContainer = document.getElementById('featuring')
-  const $form = document.getElementById('form')
-  const $home = document.getElementById('home')
 
   const $modal = document.getElementById('modal')
   const $overlay = document.getElementById('overlay')
@@ -97,7 +104,18 @@ fetch('https://randomuser.me/api/')
   const $modalTitle = $modal.querySelector('h1')
   const $modalImage = $modal.querySelector('img')
   const $modalDescription = $modal.querySelector('p')
-  
+
+  function showModal() {
+    $overlay.classList.add('active')
+    $modal.style.animation = 'modalIn .8s forwards'
+  }
+  showModal()
+
+  $hideModal.addEventListener('click', hideModal)
+  function hideModal(){
+    $overlay.classList.remove('active')
+    $modal.style.animation = 'modalOut .8s forwards'
+  }
   function videoItemtemplate(movie) {
     return (
       `<div class="primaryPlaylist">
@@ -121,6 +139,17 @@ fetch('https://randomuser.me/api/')
   renderMovieList(actionList.data.movies, $actionContainer)
   renderMovieList(dramaList.data.movies, $dramaContainer)
   renderMovieList(animationList.data.movies, $animationContainer)
+
+  function addEventClick(){
+    const $primaryPlaylist = document.getElementById('primaryPlaylist')
+    $primaryPlaylist.addEventListener('click', () => {
+      alert('click')
+    })
+    // $('div').on('click', function(){
+    // })
+  }
+
+  addEventClick();
 
 })()
 
