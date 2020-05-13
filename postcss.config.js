25dcd1e1848d013268bbe56b519d49584e746df2
brahianf <brahianf@unicauca.edu.co>
2020-05-12 20:26:12 -0500

    Instalando PostCSS

---
+++ postcss.config.js
@@ -0,0 +1,32 @@
+
+// module.exports = { 
+//     plugins: [ 
+//         // require('autoprefixer')({ 
+//             // grid:true 
+//             // }), 
+//         require('postcss-preset-env')({ 
+// 			autoprefixer: {
+//                 grid: true,
+//                 // flexbox: false,
+// 			}
+//          }) 
+//     ] 
+// }
+
+module.exports = {
+    plugins: [
+      // require('autoprefixer')({
+      //   grid: true
+      // }),
+      require('postcss-cssnext')({
+        features: {
+          autoprefixer: {
+            grid: true,
+            flexbox: false,
+          },
+          customProperties: false,
+          calc: false,
+        }
+      })
+    ]
+  }
